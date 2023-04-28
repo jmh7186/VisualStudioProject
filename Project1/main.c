2628340bf9c087d1650d85517aa24add5b55147e
jmh7186 <jmh7186@gmail.com>
2023-04-28 16:10:42 +0900

    C 포인터 연습

--- Project1/main.c
+++ Project1/main.c
@@ -1,9 +1,55 @@
 #include <stdio.h>
+void main() {
+	int i=10;
+	scanf_s("%d", &i);
+	printf("입력값: %d\n", i);
+}
+
+
+
+/*
+struct point {
+	int x;
+	int y;
+};
+
+typedef struct {
+	int age;
+	char gen;
+}person;
+
+void main() {
+	int i = 100;
+	float f = 2.2f;
+	char c = 'a';
+	person* per;
+	int x = 100;
+	int y = 200;
+
+	struct point p;
+	p.x = 300;
+	p.y = 400;
+	printf("%d, %d\n", p.x, p.y);
+
+	person saram1;
+	saram1.age = 30;
+	saram1.gen = 'F';
+
+	per = &saram1;
+	printf("%d, %c\n", per->age, per->gen);
+	printf("%d, %c\n", saram1.age, saram1.gen);
+}
+*/
+
+
+
+/*
 void prn(int *p) {
 	//main의 su 값을 1000으로 바꾸기
 	*p = 1000;
 
 }
+
 
 void main() {
 	int i;
@@ -16,6 +62,5 @@ void main() {
 	printf("%d\n", *p);
 	prn(&su);
 	printf("%d\n", su);
-
-
 }
+*/
